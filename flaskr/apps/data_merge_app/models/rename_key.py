5bffcebcfdef703c1902bc1da4d8188ef9d99d9c
椿悠人 <yuto@MacBook-Air.local>
2023-02-06 19:03:42 +0900

    modify

--- flaskr/apps/data_merge_app/models/rename_key.py
+++ flaskr/apps/data_merge_app/models/rename_key.py
@@ -1,5 +1,5 @@
 def rename_sc_data_keys(dict_row: dict[str, str]) -> dict[str, str]:
-    rename_dict_row: dict[str, str] = {}
+    rename_dict_row: dict[str, str] = dict()
     for key, value in dict_row.items():
         key = key.replace("q", "sc")
         rename_dict_row[key] = value
