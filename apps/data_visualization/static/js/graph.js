aeef1c6f308c2425d4e5902ff46489aced87dbe8
Yu0130fri <suugakunohito@gmail.com>
2023-03-21 22:38:09 +0900

    test show chart from views

--- apps/data_visualization/static/js/graph.js
+++ apps/data_visualization/static/js/graph.js
@@ -5,6 +5,7 @@ let darr = dstr.split(',');
 let dlabel = String(document.getElementById('chart_labels').value);
 let larr = dlabel.split(',');
 let ctx = document.getElementById('myChart').getContext('2d');
+console.log(larr);
 let myRadarChart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -13,24 +14,49 @@ let myRadarChart = new Chart(ctx, {
             data:darr,//app.pyのchart_data
             backgroundColor: "rgba(255,0,0,0.2)", // 線の下の塗りつぶしの色
             borderColor: "red",                   // 線の色
-            borderWidth: 2,                       // 線の幅
+            borderWidth: 1,                       // 線の幅
             label: question_num //app.pyのchart_question_num
             }
         ]
     },
+    options: {
+        responsive: true,
+        title: {                // タイトル
+            display: true,      // 表示する
+            fontSize: 20,       // タイトルのフォント
+            text: title         //app.pyのchart_title
+        },
+        scales: {                          // 軸設定
+            xAxes: [                           // Ｘ軸設定
+                {
+                    display: true,                // 表示の有無
+                    barPercentage: 0.8,           // カテゴリ幅に対する棒の幅の割合
+                    //categoryPercentage: 0.8,    // 複数棒のスケールに対する幅の割合
+                    scaleLabel: {                 // 軸ラベル
+                        display: true,                // 表示設定
+                        // labelString: '横軸ラベル', // ラベル
+                        fontColor: "red", // 文字の色
+                        fontSize: 7 // フォントサイズ
+                    },
+                    gridLines: { // 補助線
+                        display: false // 補助線なし
+                    },
+                    ticks: { // 目盛り
+                        fontColor: "red", // 目盛りの色
+                        fontSize: 14, // フォントサイズ
+                        autoSkip: false
+                    },
+                }
+            ],
+        }
+    }
     // options: {
-    //     responsive: true,
-    //     title: {                // タイトル
-    //         display: true,      // 表示する
-    //         fontSize: 20,       // タイトルのフォント
-    //         text: title         //app.pyのchart_title
-    //     },
-    //     legend: {
-    //         position: 'bottom', // 凡例の表示位置
-    //         labels: {
-    //             fontSize: 20,   // 判例のフォントサイズ
-    //         },
-    //     },
+        // legend: {
+        //     position: 'bottom', // 凡例の表示位置
+        //     labels: {
+        //         fontSize: 20,   // 判例のフォントサイズ
+        //     },
+        // },
     //     scale: {
     //         // スケールを隠す。
     //         display: true,      // メモリを表示する
--- apps/data_visualization/views.py
+++ apps/data_visualization/views.py
@@ -1,17 +1,36 @@
+from pathlib import Path
+
 from flask import Blueprint, render_template, request
+
+from .src.chart import Chart
 
 data_visualization_app = Blueprint(
     "data_visualization", __name__, template_folder="templates", static_folder="static"
 )
 
+_SAMPLE_PATH = Path(__file__).parent.parent.parent
+_DATA_DIR = _SAMPLE_PATH / "tests/test_data"
+_LAYOUT_DIR = _DATA_DIR / "test_layout/test_for_chart_data.txt"
+_RAWDATA_DIR = _DATA_DIR / "test_main_data/test_chart_data.txt"
+
 
 @data_visualization_app.route("/", methods=["GET"])
 def index():
+    chart = Chart.from_input_file(rawdata_path=_RAWDATA_DIR, layout_path=_LAYOUT_DIR)
+
+    query_data = chart.query("Q4")
+
+    # c = {
+    #     "chart_title": "グラフサンプル",  # 設問文
+    #     "chart_labels": "項目1, 項目２, 項目３, 項目４,項目５",  # 回答内容 # 自動で決まる
+    #     "chart_data": "4, 7, 8, 5, 6",  # データのカウント
+    #     "question_num": "設問番号",
+    # }
     c = {
-        "chart_title": "グラフサンプル",  # 設問文
-        "chart_labels": "項目1, 項目２, 項目３, 項目４,項目５",  # 回答内容 # 自動で決まる
-        "chart_data": "4, 7, 8, 5, 6",  # データのカウント
-        "question_num": "設問番号",
+        "chart_title": query_data.dimension.question_description,  # 設問文
+        "chart_labels": query_data.dimension.option_data,  # 回答内容 # 自動で決まる
+        "chart_data": list(query_data.measurement.chart_data.values()),  # データのカウント
+        "question_num": query_data.dimension.question_number,
     }
     return render_template("index.html", c=c)
 
